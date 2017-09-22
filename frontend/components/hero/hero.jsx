abcd75bb4c90e9ada4170a7ed44580cc2082a066
Rachel Moore <rachel@rachelmoore.net>
2017-09-22 11:31:26 -0700

    Add logo to homepage

--- frontend/components/hero/hero.jsx
+++ frontend/components/hero/hero.jsx
@@ -12,6 +12,7 @@ class Hero extends React.Component {
         <div className="text-search-container">
           <div className="logo-text">
             <h2>MeowNow</h2>
+            <img src="https://i.imgur.com/dPJ7A0L.png" height="65px" />
           </div>
           <div className="tagline-text">
             <h6>Find public cats near you!</h6>
@@ -22,7 +23,7 @@ class Hero extends React.Component {
         </div>
 
         <div className="cat-banner">
-          <img src="http://hellahoops.com/CatBanner.jpg" width="700px" />
+          <img src="http://hellahoops.com/CatBanner.jpg" width="600px;" />
         </div>
 
       </div>
