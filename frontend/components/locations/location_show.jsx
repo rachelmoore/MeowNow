9c441fbb7bf9b4edec4833c98b09b49394c50237
Rachel Moore <rachel@rachelmoore.net>
2017-09-23 17:51:26 -0700

    Fix navigation from one show page to another show page

--- frontend/components/locations/location_show.jsx
+++ frontend/components/locations/location_show.jsx
@@ -4,14 +4,22 @@ import { Link } from 'react-router-dom';
 class LocationShow extends React.Component {
   constructor(props) {
     super(props);
+    this.state = this.props;
   }
 
   componentDidMount() {
     this.props.fetchLocation(this.props.match.params.locationId);
   }
 
+  componentWillReceiveProps(newProps) {
+    if(newProps.match.params.locationId !== this.props.match.params.locationId){
+      this.props.fetchLocation(newProps.match.params.locationId);
+    }
+    this.setState(newProps);
+  }
+
   render() {
-    const location = this.props.location;
+    const location = this.state.location;
     if (location) {
     return(
       <div>
