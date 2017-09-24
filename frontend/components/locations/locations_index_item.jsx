48ec7da02e504d996e3ace0b94129f8d838f7f18
Rachel Moore <rachel@rachelmoore.net>
2017-09-23 19:28:37 -0700

    LocationIndexItem divs organized and temporary photo added

--- frontend/components/locations/locations_index_item.jsx
+++ frontend/components/locations/locations_index_item.jsx
@@ -4,9 +4,22 @@ import { Link } from 'react-router-dom';
 const LocationsIndexItem = ({ location }) => {
   return (
     <li>
-      <Link className="location-index-item-name" to={`/locations/${location.id}`}>
-        { location.location_name }
-      </Link>
+      <div className="location-index-item-main">
+        <div className="location-index-item-image">
+          <img src="https://pbs.twimg.com/media/CpPmkGJUkAAjbL6.jpg" height="100px" width="100px" />
+        </div>
+        <div id="location-index-item-main-info">
+          <Link className="location-index-item-name" to={`/locations/${location.id}`}>
+            { location.location_name }
+          </Link>
+          <p className="location-index-item-rating">5 STARS</p>
+          <p className="location-index-item-free">{location.free}</p>
+          <p className="location-index-item-adoptable">{location.adopatble}</p>
+        </div>
+      </div>
+      <div className="location-index-item-contact">
+        <p>{location.address}</p>
+      </div>
     </li>
   )
 }
