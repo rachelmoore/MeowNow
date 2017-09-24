48c8dd385ecbbe02a1a6d203038b88c08f272039
Rachel Moore <rachel@rachelmoore.net>
2017-09-24 15:18:01 -0700

    Complete locations index grid

--- frontend/components/locations/locations_index_item.jsx
+++ frontend/components/locations/locations_index_item.jsx
@@ -5,24 +5,26 @@ const LocationsIndexItem = ({ location }) => {
   return (
     <li>
       <div className="locations-index-item-all">
-      <div className="locations-index-item-image">
-        <img src="https://pbs.twimg.com/media/CpPmkGJUkAAjbL6.jpg" height="100px" width="100px" />
-      </div>
 
-      <div className="locations-index-item-main">
-        <Link className="locations-index-item-name" to={`/locations/${location.id}`}>
-          { location.location_name }
-        </Link>
-          <p className="locations-index-item-rating">5 STARS</p>
-          <p className="locations-index-item-free">{location.free}</p>
-          <p className="locations-index-item-adoptable">{location.adoptable}</p>
-      </div>
+        <div className="locations-index-item-name">
+          <Link className="locations-index-item-name-link" to={`/locations/${location.id}`}>
+            { location.location_name }
+          </Link>
+        </div>
 
-      <div className="locations-index-item-contact">
-        <p>{location.street_address}</p>
-        <p>{location.city} {location.state}</p>
+        <div className="locations-index-item-info">
+          <div className="locations-index-item-photo">
+            <img src="https://pbs.twimg.com/media/CpPmkGJUkAAjbL6.jpg" height="100px" width="100px" />
+          </div>
+          <div className="locations-index-item-details-container">
+            <div className="locations-index-item-details">
+            <p className="locations-index-item-rating">5 STARS</p>
+            <p>{location.street_address}</p>
+            <p>{location.city} {location.state}</p>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
     </li>
   )
 }
