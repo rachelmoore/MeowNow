fb84ba05a192ac51a074f938ffa0f187ee90deb2
Rachel Moore <rachel@rachelmoore.net>
2017-10-24 18:48:51 -0700

    wtf is a public cat

--- frontend/components/footer/public_cat.jsx
+++ frontend/components/footer/public_cat.jsx
@@ -8,24 +8,50 @@ class PublicCat extends React.Component {
 
     render() {
         return (
-            <div className="hero-container">
+            <div className="public-cat-container">
 
-                <div className="text-search-container">
-                    <div className="logo-text">
-                        <h2>MeowNow</h2>
-                        <img src="http://hellahoops.com/catdoodletransparent.png" height="70px" className="hero-logo-img" />
-                    </div>
-                    <div className="tagline-text">
-                        <h5>Find <span className="yellow-span">public catsssssssomg</span> near you!</h5>
-                    </div>
+                <div className="public-cat-title">
+                    <center>
+                        <h2 className="what-is-public-cat">What is a "public cat"?</h2>
+                        <p>Sounds creepy...</p>
+                    </center>
                 </div>
 
-                <div className="cat-banner">
-                    <img src="http://hellahoops.com/CatBanner.jpg" width="800px;" />
+                <div className="public-cat-story">
+                    <div className="public-cat-story-text-left">
+                        <div className="cat-text">
+                            <p>"Public Cats" are simply cats who reside at businesses that are open to the public. In the US, 
+                                these businesses tend to be bodegas, liquor stores, gardening or hardware shops, bookstores, and
+                                'cat cafes'. MeowNow also lists adoption centers such as the SPCA where people can interact with 
+                                cats and kittens up for adoption.
+                            </p>
+                            <p>Just because a location is listed as being a home to a public cat(s) does not mean that you are 
+                               guaranteed to meet a cat at said location. If you are a cat lover you already know that 
+                               cats do what they want and roam where they please! The reviews will tell you how likely you are to
+                               encounter a cat at a given location and what the cat(s) are like.
+                            </p>  
+                        </div>
+                        <div className="cat-image">
+                        </div>
+                    </div>
+                    <div className="public-cat-story-text-right">
+                    </div>
+                    <div className="public-cat-story-text-left">
+                        <div className="cat-text">
+                            <p>Most of the locations listed on MeowNow are small, local businesses. If you wander into one of them 
+                               we encourage you to buy something to support the business. In our experience
+                               most shop owners who house cats are more than happy to answer questions about their feline residents
+                               so ask away! But also make sure to be a respectful customer by purchasing something, no matter how small 
+                               the price.
+                            </p>
+                        </div>
+                        <div className="cat-image">
+                        </div>
+                    </div>
                 </div>
 
             </div>
-        )
+        );
     }
 }
 
