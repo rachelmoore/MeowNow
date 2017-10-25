6cfa08fb0c686eebf3d687a5fd66602383f449fe
Rachel Moore <rachel@rachelmoore.net>
2017-10-25 13:12:10 -0700

    Public Cat

--- frontend/components/footer/public_cat.jsx
+++ frontend/components/footer/public_cat.jsx
@@ -9,6 +9,7 @@ class PublicCat extends React.Component {
     render() {
         return (
             <div className="public-cat-container">
+                <div className="public-cat-main">
 
                 <div className="public-cat-title">
                     <center>
@@ -20,21 +21,25 @@ class PublicCat extends React.Component {
                 <div className="public-cat-story">
                     <div className="public-cat-story-text-left">
                         <div className="cat-text">
-                            <p>"Public Cats" are simply cats who reside at businesses that are open to the public. In the US, 
+                            <p>"Public Cats" are simply cats who reside at businesses that are open to the public. In the US 
                                 these businesses tend to be bodegas, liquor stores, gardening or hardware shops, bookstores, and
                                 'cat cafes'. MeowNow also lists adoption centers such as the SPCA where people can interact with 
                                 cats and kittens up for adoption.
                             </p>
-                            <p>Just because a location is listed as being a home to a public cat(s) does not mean that you are 
-                               guaranteed to meet a cat at said location. If you are a cat lover you already know that 
-                               cats do what they want and roam where they please! The reviews will tell you how likely you are to
-                               encounter a cat at a given location and what the cat(s) are like.
-                            </p>  
                         </div>
                         <div className="cat-image">
                         </div>
                     </div>
                     <div className="public-cat-story-text-right">
+                        <div className="cat-text">
+                            <p>Just because a location is listed as being home to public cat(s) does not mean that you are
+                               guaranteed to meet a cat at said location. If you are a cat lover you already know that
+                               cats do what they want and roam where they please! The reviews will tell you how likely you are to
+                               encounter a cat at a given location and what the cat(s) are like.
+                            </p>
+                        </div>
+                        <div className="cat-image">
+                        </div>
                     </div>
                     <div className="public-cat-story-text-left">
                         <div className="cat-text">
@@ -49,7 +54,7 @@ class PublicCat extends React.Component {
                         </div>
                     </div>
                 </div>
-
+                </div>
             </div>
         );
     }
