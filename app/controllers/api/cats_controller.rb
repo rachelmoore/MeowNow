eba093a62c25d162ea2ed7e211f0edd04ddda27f
Rachel Moore <rachel@rachelmoore.net>
2017-11-03 22:03:34 -0700

    Random cat controller

--- app/controllers/api/cats_controller.rb
+++ app/controllers/api/cats_controller.rb
@@ -4,12 +4,12 @@ class Api::CatsController < ApplicationController
        
     end 
 
-    # def random_cat_by_location
-    #     @cat = Cat.where(location_id: params[:location_id]).sample
-    #     unless @cat
-    #         @cat = {name: "Unknown", img_url: "http://res.cloudinary.com/df9oqycdp/image/upload/c_crop,h_382,w_382/v1506710329/Cat_Helmet_ayhx66.jpg"} 
-    #     end 
-    #     render :show
-    # end 
+    def random_cat_by_location
+        @cat = Cat.where(location_id: params[:location_id]).sample
+        unless @cat
+            @cat = {name: "Unknown", img_url: "http://res.cloudinary.com/df9oqycdp/image/upload/c_crop,h_382,w_382/v1506710329/Cat_Helmet_ayhx66.jpg"} 
+        end 
+        render :show
+    end 
 
 end
