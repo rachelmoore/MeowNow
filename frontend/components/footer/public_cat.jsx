62ad28592dcbcbe229d228a7994c87525cce1c09
Rachel Moore <rachel@rachelmoore.net>
2017-11-08 17:01:39 -0800

    Responsive with photos and temp gray backgrounds

--- frontend/components/footer/public_cat.jsx
+++ frontend/components/footer/public_cat.jsx
@@ -20,22 +20,22 @@ class PublicCat extends React.Component {
 
                      <div className="public-cat-story">
                         <div className="cat-1">
-                            <p>"Public Cats" are simply cats who reside at businesses that are open to the public. In the US 
+                            <p className="public-cat-paragraph">"Public Cats" are simply cats who reside at businesses that are open to the public. In the US 
                                 these businesses tend to be bodegas, liquor stores, gardening or hardware shops, bookstores, and
                                 'cat cafes'. MeowNow also lists adoption centers such as the SPCA where people can interact with 
                                 cats and kittens up for adoption.
                             </p>
                         </div>
                         <div className="cat-2">
-                            <img src="http://res.cloudinary.com/df9oqycdp/image/upload/c_crop,h_800,w_1200/v1510186181/catfreezer_foidyf.jpg" />
+                            <img src="http://res.cloudinary.com/df9oqycdp/image/upload/c_crop,h_800,w_1200/v1510186181/catfreezer_foidyf.jpg" height="400px" />
                         </div>
                     </div>
                     <div className="public-cat-story">
-                        <div className="cat-1">
-                            <img src="http://hellahoops.com/catdoodletransparent.png" />
+                        <div className="cat-3">
+                            <img src="http://res.cloudinary.com/df9oqycdp/image/upload/c_crop,h_800,w_1200,x_0,y_0/v1509336742/otis_o6whtz.jpg" height="400px" />
                         </div>
-                        <div className="cat-2">
-                            <p>Just because a location is listed as being home to public cat(s) does not mean that you are
+                        <div className="cat-4">
+                            <p className="public-cat-paragraph">Just because a location is listed as being home to public cat(s) does not mean that you are
                                guaranteed to meet a cat at said location. If you are a cat lover you already know that
                                cats do what they want and roam where they please! The reviews will tell you how likely you are to
                                encounter a cat at a given location and what the cat(s) are like.
@@ -43,16 +43,16 @@ class PublicCat extends React.Component {
                         </div>
                     </div>
                     <div className="public-cat-story">
-                        <div className="cat-1">
-                            <p>Most of the locations listed on MeowNow are small, local businesses. If you wander into one of them 
+                        <div className="cat-5">
+                            <p className="public-cat-paragraph">Most of the locations listed on MeowNow are small, local businesses. If you wander into one of them 
                                we encourage you to buy something to support the business. In our experience
                                most shop owners who house cats are more than happy to answer questions about their feline residents
                                so ask away! But also make sure to be a respectful customer by purchasing something, no matter how small 
                                the price.
                             </p>
                         </div>
-                        <div className="cat-2">
-                                <img src="http://hellahoops.com/catdoodletransparent.png" />
+                        <div className="cat-6">
+                            <img src="http://res.cloudinary.com/df9oqycdp/image/upload/v1510188680/catatm_xu89wg.png" height="400px" />
                         </div>
                     </div> 
             
